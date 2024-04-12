bc729e3e719cfc48aa4bf29f43e1d47b7b0cf8e6
Darius <nicoaradarius2007@gmail.com>
2024-04-13 01:43:59 +0300

    del

--- The_Cherno_series/src/Debug/CMakeFiles/program.dir/CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/program.dir/CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp.o: \
- /home/darius/Dev/C++/Opengl/The_Cherno_series/src/Debug/CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp \
- /usr/include/stdc-predef.h
