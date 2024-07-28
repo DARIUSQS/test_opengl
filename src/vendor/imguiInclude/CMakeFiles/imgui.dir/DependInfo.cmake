61e6b28d407604b92befa2231137fb056ca391ac
DARIUSQS <dariusnicoara2007@gmail.com>
2024-07-28 21:18:33 +0300

    d

---
+++ src/vendor/imguiInclude/CMakeFiles/imgui.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/darius/Dev/cpp/test_opengl/src/vendor/imgui/src/imgui/backends/imgui_impl_glfw.cpp" "vendor/imgui/CMakeFiles/imgui.dir/src/imgui/backends/imgui_impl_glfw.cpp.o" "gcc" "vendor/imgui/CMakeFiles/imgui.dir/src/imgui/backends/imgui_impl_glfw.cpp.o.d"
+  "/home/darius/Dev/cpp/test_opengl/src/vendor/imgui/src/imgui/backends/imgui_impl_opengl3.cpp" "vendor/imgui/CMakeFiles/imgui.dir/src/imgui/backends/imgui_impl_opengl3.cpp.o" "gcc" "vendor/imgui/CMakeFiles/imgui.dir/src/imgui/backends/imgui_impl_opengl3.cpp.o.d"
+  "/home/darius/Dev/cpp/test_opengl/src/vendor/imgui/src/imgui/imgui.cpp" "vendor/imgui/CMakeFiles/imgui.dir/src/imgui/imgui.cpp.o" "gcc" "vendor/imgui/CMakeFiles/imgui.dir/src/imgui/imgui.cpp.o.d"
+  "/home/darius/Dev/cpp/test_opengl/src/vendor/imgui/src/imgui/imgui_demo.cpp" "vendor/imgui/CMakeFiles/imgui.dir/src/imgui/imgui_demo.cpp.o" "gcc" "vendor/imgui/CMakeFiles/imgui.dir/src/imgui/imgui_demo.cpp.o.d"
+  "/home/darius/Dev/cpp/test_opengl/src/vendor/imgui/src/imgui/imgui_draw.cpp" "vendor/imgui/CMakeFiles/imgui.dir/src/imgui/imgui_draw.cpp.o" "gcc" "vendor/imgui/CMakeFiles/imgui.dir/src/imgui/imgui_draw.cpp.o.d"
+  "/home/darius/Dev/cpp/test_opengl/src/vendor/imgui/src/imgui/imgui_tables.cpp" "vendor/imgui/CMakeFiles/imgui.dir/src/imgui/imgui_tables.cpp.o" "gcc" "vendor/imgui/CMakeFiles/imgui.dir/src/imgui/imgui_tables.cpp.o.d"
+  "/home/darius/Dev/cpp/test_opengl/src/vendor/imgui/src/imgui/imgui_widgets.cpp" "vendor/imgui/CMakeFiles/imgui.dir/src/imgui/imgui_widgets.cpp.o" "gcc" "vendor/imgui/CMakeFiles/imgui.dir/src/imgui/imgui_widgets.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
